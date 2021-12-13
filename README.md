ef85f2ca2de36b57c9b9bfbb6468fb6835e20965
shivshukla72 <shuklashiv72@gmail.com>
2021-12-13 13:38:49 +0530

    update files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Loopstudios--landing-page
-front end mentor
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#ls-nav__hamburger-icon').on('click', function () {
+        $('body').toggleClass('show');
+        // $('.ls-nav__close').css('display', 'block');
+        // $('.ls-nav__open').css('display', 'none');
+    })
+
+})
